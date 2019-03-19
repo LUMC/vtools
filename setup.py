896ae2ed8cdf8f207850dd9cb28b29b75443b025
Sander Bollen <sander@sndrtj.eu>
2019-03-19 10:17:22 +0100

    use numpy.get_includes in C-extensions

--- setup.py
+++ setup.py
@@ -6,23 +6,34 @@ setup.py
 :copyright: (c) 2018 Leiden University Medical Center
 :license: MIT
 """
-
 from os.path import abspath, dirname, join
 
 from setuptools import setup, find_packages
-from Cython.Build import cythonize
 
-from vtools import __version__
 
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    raise NotImplementedError("Installing cython on the fly not yet supported")
+
+
+try:
+    import numpy as np
+except ImportError:
+    raise NotImplementedError("Installing numpy on the fly not yet supported")
 
 readme_file = join(abspath(dirname(__file__)), "README.md")
 with open(readme_file) as desc_handle:
     long_desc = desc_handle.read()
 
+# create extensions and add numpy includes to all of them.
+cython_extensions = cythonize("vtools/*.pyx")
+for ext in cython_extensions:
+    ext.include_dirs.append(np.get_include())
 
 setup(
     name="vtools",
-    version=__version__,
+    version="0.0.1",
     description="Various tools operating over VCF files",
     author="Sander Bollen",
     author_email="a.h.b.bollen@lumc.nl",
@@ -46,5 +57,5 @@ setup(
     classifiers=[
         "Topic :: Scientific/Engineering :: Bio-Informatics"
     ],
-    ext_modules=cythonize("vtools/*.pyx")
+    ext_modules=cython_extensions
 )
