6562310687801d888c0afbfd1dc90cef4aee9006
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2019-08-27 11:30:00 +0200

    Add real test for site_concordancy

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,16 @@
-def func(x):
-    return x + 1
+import pytest
+
+from vtools.evaluate import site_concordancy
+
+from cyvcf2 import VCF
 
 
-def test_answer():
-    assert func(41) == 42
+@pytest.fixture
+def known():
+    bla =VCF('tests/cases/known1.vcf.gz', gts012=True)
+    return bla
+
+def test_evaluate(known):
+    d, disc = site_concordancy(known, known, ["known1"], ["known1"])
+    assert d['total_sites'] == 3
+    assert not disc
