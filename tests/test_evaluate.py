26922303b9850fc52a1062bca7bed22983f5da5a
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2019-08-27 16:19:49 +0200

    Add testcase for every field in the output

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -7,11 +7,20 @@ from cyvcf2 import VCF
 
 @pytest.fixture
 def known_concordant():
-    known = '/home/rrvandenberg/devel/vtools/tests/cases/gatk.vcf.gz'
+    known = 'tests/cases/gatk.vcf.gz'
     d, disc = site_concordancy(VCF(known, gts012=True), VCF(known,
                                                             gts012=True),
                                ['NA12878'], ['NA12878'], min_gq=0,
                                min_dp=0)
+    return d
+
+
+@pytest.fixture
+def blank_NA12878():
+    filename = 'tests/cases/gatk.vcf.gz'
+    d, disc = site_concordancy(VCF(filename, gts012=True), VCF(filename,
+                                                               gts012=True),
+                               ['NA12878'], ['BLANK'], min_gq=0, min_dp=0)
     return d
 
 
@@ -29,3 +38,31 @@ def test_alleles_considered(known_concordant):
 
 def test_alleles_het_concordant(known_concordant):
     assert known_concordant['alleles_het_concordant'] == 42
+
+
+def test_alleles_hom_alt_concordant(known_concordant):
+    assert known_concordant['alleles_hom_alt_concordant'] == 18
+
+
+def test_alleles_hom_ref_concordant(known_concordant):
+    assert known_concordant['alleles_hom_ref_concordant'] == 14
+
+
+def test_alleles_concordant(known_concordant):
+    assert known_concordant['alleles_concordant'] == 74
+
+
+def test_alleles_discordant(known_concordant):
+    assert known_concordant['alleles_discordant'] == 0
+
+
+def test_alleles_no_call(blank_NA12878):
+    assert blank_NA12878['alleles_no_call'] == 8
+
+
+def test_alleles_low_qual(known_concordant):
+    assert known_concordant['alleles_low_qual'] == 0
+
+
+def test_alleles_low_depth(known_concordant):
+    assert known_concordant['alleles_low_depth'] == 0
